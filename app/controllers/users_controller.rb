1d83b3e86b5b3bf2936d42ede6172ce6159f9543
Garrett <garres4@vt.edu>
2021-12-19 13:33:45 -0700

    commit tree b0825c2769a6ac7cda12300bf8cce9c60ae63eef

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+
+    before_action :find_user
+
+    def show
+        render json: @user, include: :items
+    end
+
+    private
+
+    def find_user
+        @user = User.find(params[:id])
+    end
+
 end
